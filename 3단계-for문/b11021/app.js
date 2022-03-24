8f8464f7496e8afd00b9dd38f79199ab507fb521
Dong-yeong0 <wjdehddud77@naver.com>
2022-03-24 23:41:00 +0900

    for문 3문제 2741, 2742 11021 피곦...

---
+++ 3단계-for문/b11021/app.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+//const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const filePath = "./input.txt";
+
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+const valueArray = [];
+for (let i = 1; i <= +input[0]; i++) {
+   const tempValue = input[i].split(" ").map((item) => +item);
+   valueArray.push({
+      "A": tempValue[0],
+      "B": tempValue[1]
+   });
+}
+
+solution(+input[0], valueArray);
+
+function solution(T, valueArray) {
+   let result = "";
+   for (let i = 0; i < T; i++) {
+      const A = valueArray[i].A;
+      const B = valueArray[i].B;
+      //result += "Case #" + (i + 1) + ": " + (A + B) + "\n";
+      result += `Case #${i + 1}: ${A + B} \n`;
+   }
+   console.log(result);
+}
---
+++ 3단계-for문/b2742/app.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+//const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const filePath = "./input.txt";
+
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+solution(+input[0]);
+
+function solution(N) {
+   let result = "";
+   for (let i = N; i >= 1; i--) {
+      result += i + "\n";
+   }
+   console.log(result);
+}
+// 마지막에 \n 더 붙는데 맞추네?
+// 이 문제 for 안에서 console 찍으면 시간초과 걸리고 한번에 담아서 해야되는 문제
