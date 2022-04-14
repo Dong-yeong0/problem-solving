aad4847ccbafac90d2a39b147f31461b284cf0bc
Dong-yeong0 <73709156+Dong-yeong0@users.noreply.github.com>
2022-04-14 18:28:56 +0900

    add 10809 find alphabet

--- 6단계-문자열/b11720/app.js
+++ 6단계-문자열/b11720/app.js
@@ -1,12 +1,9 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const input = require("fs").readFileSync("/dev/stdin").toString();
 
-let casesCount = Number(input[0]);
-let cases = input[1].split('');
-let sum = 0;
+const result = [];
 
-for (let i = 0; i < casesCount; i++) {
-  sum = sum + Number(cases[i]);
+for (let i = 97; i <= 122; i++) {
+   result.push(input.indexOf(String.fromCharCode(i)));
 }
 
-console.log(sum);
+console.log(result.join(" "));
