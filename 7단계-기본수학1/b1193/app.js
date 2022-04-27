af5aa7120223d1033307daf3577c57f7ce4acd95
Dong-yeong0 <73709156+Dong-yeong0@users.noreply.github.com>
2022-04-27 22:57:36 +0900

    add 1193 분수

---
+++ 7단계-기본수학1/b1193/app.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+//const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const filePath = "./input.txt";
+
+let input = fs.readFileSync(filePath).toString().map((item) => +item);
+
+solution(input);
+
+function solution(input) {
+	let line = 0;
+	let maxValue = 0;
+
+	while(maxValue < N) {
+		line++;
+		maxValue += line;
+	}
+	
+	let index = N - (maxValue - line);
+	
+	if(line % 2 == 0) {
+		child = index;
+		parents = (line + 1) -child;
+	} else {	
+		child = (line + 1) - index;
+		parents = (line + 1) - child;
+	}
+	
+	console.log(`${child}/${parents});
+
+}
