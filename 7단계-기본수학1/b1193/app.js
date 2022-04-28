9dbcda3f865274a5d3bbbd1e056d18a945e39c5d
Dong-yeong0 <73709156+Dong-yeong0@users.noreply.github.com>
2022-04-28 22:04:20 +0900

    modify 1193

--- 7단계-기본수학1/b1193/app.js
+++ 7단계-기본수학1/b1193/app.js
@@ -1,3 +1,5 @@
+
+// 왜 틀린거지
 const fs = require("fs");
 //const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const filePath = "./input.txt";
@@ -25,7 +27,26 @@ function solution(input) {
 		parents = (line + 1) - child;
 	}
 	
-	console.log(`${child}/${parents});
+	console.log(`${child}/${parents}`);
 
 }
-// 왜 틀린거지
+
+
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+let number = Number(input);
+let value = 1;
+while (true) {
+	number -= value;
+	if (number <= 0) {
+		number += value;
+		break;
+	}
+	value++;
+}
+
+if (value % 2 === 1) {
+	console.log(`${value - (number - 1)}/${1 + (number - 1)}`);
+} else {
+	console.log(`${1 + (number - 1)}/${value - (number - 1)}`);
+}
