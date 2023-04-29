d03462d203dc563aaada444c1e4dbd3e88238a7e
dong-yeong0 <wjdehddud77@naver.com>
2023-04-29 21:07:12 +0900

    add p25314 영수증

--- python/3단계-반복문/p25314.py
+++ python/3단계-반복문/p25314.py
@@ -1,4 +1,8 @@
-N = int(input())
-for _ in range(N//4):
-    print("long", end=" ")
-print("int")
+x = int(input())
+total_price = 0
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    total_price += a * b
+
+print("Yes") if total_price == x else print("No")
