83c978eca7f23bb74d919018464c3cae43e23112
dong-yeong0 <wjdehddud77@naver.com>
2023-05-03 21:33:00 +0900

    add 10810 공 넣기

--- python/4단계-1차원_배열/10807.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-n_arr = list(map(int, input().split()))
-v = int(input())
-
-print(n_arr.count(v))
---
+++ python/4단계-1차원_배열/p10807.py
@@ -0,0 +1,5 @@
+n = int(input())
+n_arr = list(map(int, input().split()))
+v = int(input())
+
+print(n_arr.count(v))
