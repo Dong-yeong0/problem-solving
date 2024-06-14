e64f273be27cfe2365db6c2d462b1ebaa7808f27
Dong-yeong0 <kevin@beomhoit.com>
2024-06-14 21:18:07 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -0,0 +1,8 @@
+def solution(my_string, indices):
+    indices = sorted(indices)
+    answer = []
+    for i in range(len(my_string)):
+        if i not in indices:
+            answer.append(my_string[i])
+    
+    return ''.join(answer)
