292425d0787e8a990533d753c741aae8b5035da8
Dong-yeong0 <kevin@beomhoit.com>
2024-09-01 14:40:49 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.51 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,4 +1,5 @@
 def solution(my_string):
+    """
     char_map = {}
     for i in  range(ord('Z') - ord('A') + 1):
         char_map[chr(ord('A') + i)] = 0
@@ -10,3 +11,12 @@ def solution(my_string):
             char_map[char] += 1
     
     return list(char_map.values())
+    """
+    answer = [0] * 52
+    for word in my_string:
+        if ord("A") <= ord(word) <= ord("Z"):
+            answer[ord(word) - ord("A")] += 1
+        else:
+            answer[ord(word) - ord("a") + 26] += 1
+    
+    return answer    
