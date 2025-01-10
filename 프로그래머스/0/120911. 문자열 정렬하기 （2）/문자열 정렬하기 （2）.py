fd6c0756446ceaff5e1a1862eb11153623d19089
Dong-yeong0 <kevin@beomhoit.com>
2025-01-10 12:55:08 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,2 +1,14 @@
+"""
 def solution(my_string):
     return "".join(sorted(list(my_string.lower())))
+"""
+
+def solution(my_string):
+    answer = []
+    for char in my_string:
+        if ord(char) >= ord("A") and ord(char) <= ord("Z"):
+            answer.append(chr(ord(char) + 32)) # 대문자를 ascii로 바꾸어 32를 더하면 해당 문자의 소문자로 변환이 가능하다.
+        else:
+            answer.append(char)
+
+    return "".join(sorted(answer))
