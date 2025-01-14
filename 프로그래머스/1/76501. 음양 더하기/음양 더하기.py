a24a22fb1e5595fc3eaf1f1432f3f8e7ddecacca
Dong-yeong0 <kevin@beomhoit.com>
2025-01-14 12:35:24 +0900

    [level 1] Title: 음양 더하기, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,3 +1,4 @@
+"""
 def solution(absolutes, signs):
     answer = 0
     for i in range(len(absolutes)):
@@ -5,5 +6,16 @@ def solution(absolutes, signs):
             answer += absolutes[i]
         else:
             answer -= absolutes[i]
+    
+    return answer
+"""
+
+def solution(absolutes, signs):
+    answer = 0
+    for absolute, sign in zip(absolutes, signs):
+        if sign:
+            answer += absolute
+        else:
+            answer -= absolute
     
     return answer
