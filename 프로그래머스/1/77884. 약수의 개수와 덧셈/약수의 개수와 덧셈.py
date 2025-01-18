3b3bf46dbbba8a3aa33ed934aee0773677521f75
Dong-yeong0 <kevin@beomhoit.com>
2025-01-18 16:30:19 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,3 +1,4 @@
+"""
 def getDivisors(n):
     result = []
     for i in range(1, int(n ** (1/2) + 1)):
@@ -18,4 +19,19 @@ def solution(left, right):
         else:
             answer -= i
         
+    return answer
+"""
+
+# 완전제곱수(약수의 갯수가 홀수 인 경우)로 접근
+def is_perfect_number(n):
+    return int(n ** 0.5) ** 2 == n
+
+def solution(left, right):
+    answer = 0
+    for i in range(left, right + 1):
+        if is_perfect_number(i):
+            answer -= i
+        else:
+            answer += i
+            
     return answer
