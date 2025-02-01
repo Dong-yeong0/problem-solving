2828c4aa276ce63859a3c5d34215d2c197e5c6c2
Dong-yeong0 <kevin@beomhoit.com>
2025-02-01 20:07:25 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,34 @@
+WITH AVG_GRADE AS (
+    SELECT 
+        EMP_NO, 
+        ROUND(AVG(SCORE), 2) AS AVG_SCORE
+    FROM 
+        HR_GRADE
+    WHERE 
+        YEAR = 2022
+    GROUP BY 
+        EMP_NO
+)
+SELECT 
+    E.EMP_NO, 
+    E.EMP_NAME, 
+    CASE 
+        WHEN A.AVG_SCORE >= 96 THEN 'S'
+        WHEN A.AVG_SCORE >= 90 THEN 'A'
+        WHEN A.AVG_SCORE >= 80 THEN 'B'
+        ELSE 'C'
+    END AS GRADE,
+    CASE 
+        WHEN A.AVG_SCORE >= 96 THEN ROUND(E.SAL * 0.2)
+        WHEN A.AVG_SCORE >= 90 THEN ROUND(E.SAL * 0.15)
+        WHEN A.AVG_SCORE >= 80 THEN ROUND(E.SAL * 0.1)
+        ELSE 0
+    END AS BONUS
+FROM 
+    HR_EMPLOYEES E
+JOIN 
+    AVG_GRADE A 
+ON 
+    E.EMP_NO = A.EMP_NO
+ORDER BY 
+    E.EMP_NO
